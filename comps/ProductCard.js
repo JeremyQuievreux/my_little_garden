d05333dae2ab6b2871ba470659c296284191e8d4
Quievreux <jquievreux.devpro@gmail.com>
2022-04-08 09:45:20 +0200

    Update ProductCard.js

--- comps/ProductCard.js
+++ comps/ProductCard.js
@@ -8,11 +8,13 @@ function ProductCard({product}) {
 
     const [ quantity, setQuantity ] = useState(1)
 
-    const addQuantity = () => {
+    const addQuantity = (e) => {
+        e.preventDefault()
         setQuantity(quantity + 1)
     }
 
-    const removeQuantity = () => {
+    const removeQuantity = (e) => {
+        e.preventDefault()
         if (quantity == 1) {
             null
         } else {
@@ -20,25 +22,35 @@ function ProductCard({product}) {
         }
     }
 
-    const showBuyProduct = () => {
+    const showBuyProduct = (e) => {
+        e.preventDefault()
         console.log(product.name , quantity, "pour" , product.price * quantity, " €");
         setQuantity(1)
     }
 
   return (
-    <div className={styles.container}>
-        <div className={styles.product_header}>
-            <p>{product.name}</p>
+        <div className={styles.container}>
+        <Link href={"/products/"+ product._id}>
+            <a>
+                <div className={styles.product_header}>
+                    <p>{product.name}</p>
+                </div>
+                <div className={styles.product_body}>
+                    <img src={product.url_pic} alt="" />
+                </div>
+                <div className={styles.product_footer}>
+                    <p>Quantité : </p>
+                    <div>
+                        <button className={styles.quantity_button} onClick={(e)=> removeQuantity(e)}>
+                        <p>-</p>
+                        </button> {quantity} <button className={styles.quantity_button} onClick={(e) => addQuantity(e)}>+</button>
+                    </div>
+                    <p>{product.price} €</p>
+                    <button onClick={(e) => showBuyProduct(e)}>Ajouter au panier</button>
+                </div>
+            </a>
+        </Link>
         </div>
-        <div className={styles.product_body}>
-            <img src={product.url_pic} alt="" />
-        </div>
-        <div className={styles.product_footer}>
-            <p>Quantité :   <button className={styles.quantity_button} onClick={()=> removeQuantity()}>-</button> {quantity} <button className={styles.quantity_button} onClick={() => addQuantity()}>+</button></p>
-            <p>{product.price} €</p>
-            <button onClick={() => showBuyProduct()}>Ajouter au panier</button>
-        </div>
-    </div>
   )
 }
 

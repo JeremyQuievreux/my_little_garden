b9649d1b1fd76f8754d8541732bfa9c85c13bcc1
Quievreux <jquievreux.devpro@gmail.com>
2022-04-26 13:56:50 +0200

    start of findbar with sort and filter / some bug with "all" and "all"

---
+++ comps/FilterBar.js
@@ -0,0 +1,101 @@
+import React ,{ useState , useEffect } from 'react'
+import styles from '../styles/comps/FilterBar.module.scss'
+
+function FilterBar({products, setAllProducts, allProducts}) {
+
+  const [ filter, setFilter ] = useState("all");
+  const [ sorter, setSorter ] = useState("all");
+
+  //filter products
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
+  //sorter products
+  const handleSorterChange = (e) => {
+    setSorter(e.target.value)
+  }
+
+  const filterFunc = (products, filter) => {
+    if(filter === "all") {
+      return products
+    } else {
+      const filteredProduct = products.filter(product => product.famille == filter)
+      return filteredProduct
+    }
+  }
+
+  const sortByAlpha = (a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+  const sortBySmallPrice = (a, b) => {
+    if (a.price < b.price) {
+      return -1;
+    }
+    if (a.price > b.price) {
+      return 1;
+    }
+    return 0;
+  }
+  const sortByBigPrice = (a, b) => {
+    if (b.price < a.price) {
+      return -1;
+    }
+    if (b.price > a.price) {
+      return 1;
+    }
+    return 0;
+  }
+  const sortFunc = (products, sorter) => {
+    if (sorter == "all") {
+      const sortedProducts = products.sort(sortByAlpha)
+      return sortedProducts
+    }
+    if (sorter == "smallprice") {
+      const sortedProducts = products.sort(sortBySmallPrice)
+      return sortedProducts
+    }
+    if (sorter == "bigprice") {
+      const sortedProducts = products.sort(sortByBigPrice)
+      return sortedProducts
+    }
+  }
+
+
+  useEffect(() => {
+    const filteredProducts = filterFunc(products, filter)
+    const sortedProducts = sortFunc(filteredProducts, sorter)
+    setAllProducts(sortedProducts)
+  },[filter,sorter])
+
+  
+
+  return (
+    <div className={styles.filterbar_container}>
+        <label htmlFor="filter">Filtrer par :</label>
+        <select name="filter" id="filter"  value={filter} onChange={(e) => handleFilterChange(e)}>
+            <option value="all">Tous les produits</option>
+            <optgroup label='Famille'>
+              <option value="aromatique">Plantes Aromatiques</option>
+              <option value="fruit">Legumes Fruits</option>
+              <option value="feuille">Legumes Feuille</option>
+              <option value="graine">Legumes Graine</option>
+              <option value="racine">Legumes Racine</option>
+            </optgroup>
+        </select>
+        <label htmlFor="sorter">Trier par :</label>
+        <select name="sorter" id="sorter" value={sorter} onChange={(e) => handleSorterChange(e)}>
+            <option value="all">---</option>
+            <option value="smallprice">Prix croissant</option>
+            <option value="bigprice">Prix décroissant</option>
+        </select>
+    </div>
+  )
+}
+
+export default FilterBar
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,13 @@
 //imports
   //base React + style
-import React  from 'react'
+import React, { useState , useEffect}  from 'react'
 import styles from '../../styles/pages/Products.module.scss'
   //utils for connect to db
 import Product from '../../models/Product';
 import dbConnect from '../../utils/dbConnect';
   //Component
 import ProductCard from '../../comps/ProductCard';
+import FilterBar from '../../comps/FilterBar';
 //pre rendering
 export async function getStaticProps() {
 
@@ -23,11 +24,26 @@ export async function getStaticProps() {
 //component
 function products({products}) {
 
+  const sortByAlpha = (a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const orderedProducts = products.sort(sortByAlpha)
+
+  const [ allProducts, setAllProducts ] = useState(products)
+
   return (
     <div className={styles.container}>
         <h1>Nos Produits : </h1>
+        <FilterBar products={orderedProducts} setAllProducts={setAllProducts} allProducts={allProducts}/>
         <div className={styles.products_container}>
-          {products.map((product) => {
+          {allProducts.map((product) => {
             return(
               <ProductCard key={product._id} product={product}/>
             )
